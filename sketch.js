765b2cdf840fba5b583871dcce7c1a7a393d7e1f
simsonchen <simson123123410@gmail.com>
2020-01-13 17:46:20 +0100

    1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
-function preload(){
-  // put preload code here
+
+var sliderSz;
+var sliderSp;
+var sliderR;
+var sliderG;
+var sliderB;
+
+function setup(){
+  // noCanvas();
+  createCanvas(windowWidth,windowHeight);
+  background('red');
+
+
+// size createConvolver(
+createP('Size');
+  sliderSz = createSlider(10, 300, 50, 10);
+
+
+// shape controll
+createP('shape');
+sliderSp=createSlider(0, 1, 0.7, 0);
+
+// color controll
+createP('Color');
+  sliderR = createSlider(0, 255, 125);
+  // sliderR.position(50, windowHeight / 2 + 75);
+createP();
+  sliderG = createSlider(0, 255, 125);
+  // sliderG.position(50, windowHeight / 2 + 125);
+createP();
+  sliderB = createSlider(0, 255, 125);
+  // sliderB.position(50, windowHeight / 2 + 175);
+
 }
 
-function setup() {
-  // put setup code here
-}
 
-function draw() {
-  // put drawing code here
+
+// access to draw mode of instance
+function draw(){
+   // fill background, draw ellipse
+   rectMode(CENTER);
+   fill(100,100,124);
+   // ellipse(width/2,height/2,100);
+   // rect(mouseX,mouseY,30,100,37.5);
+      rect(mouseX,mouseY,sliderSz,sliderSz,sliderSp*sliderSz/2);
 }
